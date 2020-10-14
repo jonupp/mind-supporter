6a147c917a1bf2263a97147dddb3dbc99ed169bc
jonupp <jonas.knupp@gmx.ch>
2020-10-14 09:14:36 +0200

    Add click on ToDo

--- app/src/main/java/ch/ost/rj/mge/mind_supporter/ToDosViewHolder.java
+++ app/src/main/java/ch/ost/rj/mge/mind_supporter/ToDosViewHolder.java
@@ -1,5 +1,6 @@
 package ch.ost.rj.mge.mind_supporter;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,6 +20,15 @@ public class ToDosViewHolder extends RecyclerView.ViewHolder{
         this.finishDate = finishDate;
         this.image = image;
         this.constraintLayout = parent.findViewById(R.id.to_do_list_item_layout);
+        constraintLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent editNoteIntent = new Intent(App.getContext(), NewToDo.class);
+                editNoteIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //TODO: Add Parameters (edit = true, ToDo object or identifier for ToDo object)
+                App.getContext().startActivity(editNoteIntent);
+            }
+        });
     }
 
 }
