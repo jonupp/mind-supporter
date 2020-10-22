191961b99847a7d99657d9e5bda1b51113270846
jonupp <jonas.knupp@gmx.ch>
2020-10-22 17:54:48 +0200

    Add notifications
    Check every 15 minutes if a todo has to be done soon

--- app/src/main/java/ch/ost/rj/mge/mind_supporter/App.java
+++ app/src/main/java/ch/ost/rj/mge/mind_supporter/App.java
@@ -1,6 +1,9 @@
 package ch.ost.rj.mge.mind_supporter;
 
 import android.app.Application;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.content.Context;
 
 public class App extends Application {
@@ -11,8 +14,19 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = this;
+        scheduleJob(this);
+
     }
     public static Context getContext(){
         return mContext;
+    }
+
+    public static void scheduleJob(Context context) {
+        ComponentName serviceComponent = new ComponentName(context, NotificationJobService.class);
+        JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
+        builder.setPeriodic(900000);
+
+        JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
+        jobScheduler.schedule(builder.build());
     }
 }
---
+++ app/src/main/java/ch/ost/rj/mge/mind_supporter/NotificationJobService.java
@@ -0,0 +1,84 @@
+package ch.ost.rj.mge.mind_supporter;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import androidx.core.app.NotificationCompat;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class NotificationJobService extends JobService {
+    NotificationManager mNotifyManager;
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    public void createNotificationChannel() {
+
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,"Mind Supporter",NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifications for pending todos.");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        createNotificationChannel();
+        String title = null;
+        try {
+            FileInputStream fis = new FileInputStream(new File(App.getContext().getFilesDir(), "todos"));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ArrayList<ToDo> toDoArrayList = (ArrayList<ToDo>) ois.readObject();
+            ois.close();
+            fis.close();
+
+            LocalDateTime now = LocalDateTime.now();
+            for(ToDo t : toDoArrayList){
+                long timeDelta = ChronoUnit.MINUTES.between(now, t.getDueDateTime());
+                if(timeDelta > 0 &&  timeDelta < t.getDurationMinutes()+20 ){ //Dont send notification for past todos
+                    title = t.getTitle();
+                    break;
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if(title == null){
+            return false;
+        }
+
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class),
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Mind Supporter")
+                .setContentText("ToDo has to be done: " + title)
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.image_placeholder)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+
+        mNotifyManager.notify(0, builder.build());
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return true;
+    }
+}
